dcc76c7f57179e452b642a72307c97610799a767
AbdelhamidKHELLADI <126398352+AbdelhamidKHELLADI@users.noreply.github.com>
2024-02-03 14:05:42 +0100

    Update morseCode.ino

--- TP1/morseCode.ino
+++ TP1/morseCode.ino
@@ -12,10 +12,13 @@ const char* morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", 
 const char chars[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 void trigger(int duration){
+    void trigger(int duration){
     digitalWrite(led, HIGH); 
-    tone(piezo, 600,duration); 
+    tone(piezo, 600); 
     delay(duration); 
+  	noTone(piezo);
     digitalWrite(led, LOW);
+}
 }
 void setup()
 {
